d6b89f75565194f7e4da27359cefaf7fc9418144
Friedrich Kiltz <f@kiltz.de>
2019-10-16 17:58:08 +0200

    Etwas umfangreichere Methode

--- jax-rs-server/src/main/java/de/kiltz/rest/v1/KommunikationsRestService.java
+++ jax-rs-server/src/main/java/de/kiltz/rest/v1/KommunikationsRestService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.QueryParam;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 /**
  * @author tz
@@ -30,5 +31,20 @@ public class KommunikationsRestService {
     public String pingPlain(@QueryParam("s") String txt) {
         txt = txt == null ? "NULL" : txt;
         return txt.toUpperCase();
+    }
+
+    @GET
+    @Path("suche")
+    @Produces("text/plain")
+    @ApiOperation(value = "Such-Beispiel", produces = "text/plain")
+    public String suche(@ApiParam(
+            value = "Suchbegriff für die Volltextsuche.",
+            required = true)
+                        @QueryParam("s") String suchBegriff,
+                        @ApiParam(
+                                value = "Umgebung in der gesucht werden soll.",
+                                required = true, allowableValues = "Lokal, Global", defaultValue = "Lokal")
+                        @QueryParam("umgebung") String umgebung) {
+        return String.format("suche nach %s in der Umgebung %s", suchBegriff, umgebung);
     }
 }
