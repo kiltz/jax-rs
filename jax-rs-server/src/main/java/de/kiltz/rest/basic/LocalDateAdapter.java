037cedb9003ddddd243b43d751c955e994530ec2
Friedrich Kiltz <Friedrich.Kiltz@ofdka.bwl.de>
2019-11-07 11:00:27 +0100

    LocalDate-Beispiel mit Adapter

--- jax-rs-server/src/main/java/de/kiltz/rest/basic/LocalDateAdapter.java
+++ jax-rs-server/src/main/java/de/kiltz/rest/basic/LocalDateAdapter.java
@@ -11,13 +11,13 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
     @Override
     public LocalDate unmarshal(String dateString) throws Exception {
-        System.out.println("Unmarshal: "+dateString);
+        System.out.println("Unmarshal: " + dateString);
         return LocalDate.parse(dateString, DateTimeFormatter.ofPattern("dd.MM.YYYY"));
     }
 
     @Override
     public String marshal(LocalDate localDate) throws Exception {
-        System.out.println("Marshal: "+localDate);
+        System.out.println("Marshal: " + localDate);
         return DateTimeFormatter.ofPattern("dd.MM.YYYY").format(localDate);
     }
 }
--- jax-rs-server/src/main/java/de/kiltz/rest/basic/RootDatenTypen.java
+++ jax-rs-server/src/main/java/de/kiltz/rest/basic/RootDatenTypen.java
@@ -1,106 +1,120 @@
 package de.kiltz.rest.basic;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
-//@XmlRootElement
-//@XmlAccessorType(XmlAccessType.FIELD)
-@JsonDeserialize(using = LocalDateDeserializer.class)
-@JsonSerialize(using = LocalDateSerializer.class)
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class RootDatenTypen {
-	private String txt;
-	private int zahl;
-	private float gleitZahl;
-//	@XmlJavaTypeAdapter(LocalDateAdapter.class)
-	private Date datum;
-	private List<String> liste;
-	private Map<Integer, String> mappe;
-	private List<TeilDatenTypen> komplexeListe;
-	
-	public RootDatenTypen() {
-		double d = Math.random();
-		setTxt("Text 4: "+d);
-		setZahl((int)(d*6+1));
-		setGleitZahl((float)d);	
-		datum = new Date();
-		liste = new ArrayList<String>();
-		liste.add("Eins");
-		liste.add("Zwei");
-		liste.add("Drei");
-		
-		mappe = new HashMap<Integer, String>();
-		mappe.put(1, "Jan");
-		mappe.put(2, "Feb");
-		mappe.put(3, "Mar");
-		mappe.put(4, "Apr");
-		
-		komplexeListe = new ArrayList<TeilDatenTypen>();
-		komplexeListe.add(new TeilDatenTypen());
-		komplexeListe.add(new TeilDatenTypen());
-		komplexeListe.add(new TeilDatenTypen());
-	}
-	public String getTxt() {
-		return txt;
-	}
-	public void setTxt(String txt) {
-		this.txt = txt;
-	}
-	public int getZahl() {
-		return zahl;
-	}
-	public void setZahl(int zahl) {
-		this.zahl = zahl;
-	}
-	public float getGleitZahl() {
-		return gleitZahl;
-	}
-	public void setGleitZahl(float gleitZahl) {
-		this.gleitZahl = gleitZahl;
-	}
-	public Date getDatum() {
-		return datum;
-	}
-	public void setDatum(Date datum) {
-		this.datum = datum;
-	}
-	public List<String> getListe() {
-		return liste;
-	}
-	public void setListe(List<String> liste) {
-		this.liste = liste;
-	}
-	public Map<Integer, String> getMappe() {
-		return mappe;
-	}
-	public void setMappe(Map<Integer, String> mappe) {
-		this.mappe = mappe;
-	}
-	public List<TeilDatenTypen> getKomplexeListe() {
-		return komplexeListe;
-	}
-	public void setKomplexeListe(List<TeilDatenTypen> komplexeListe) {
-		this.komplexeListe = komplexeListe;
-	}
+    private String txt;
+    private int zahl;
+    private float gleitZahl;
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
+    private LocalDate datum;
+    private List<String> liste;
+    private Map<Integer, String> mappe;
+    private List<TeilDatenTypen> komplexeListe;
 
-	@Override
-	public String toString() {
-		final StringBuilder sb = new StringBuilder("DatenObjekt{");
-		sb.append("txt='").append(txt).append('\'');
-		sb.append(", zahl=").append(zahl);
-		sb.append(", gleitZahl=").append(gleitZahl);
-		sb.append(", datum=").append(datum);
-		sb.append(", liste=").append(liste);
-		sb.append(", mappe=").append(mappe);
-		sb.append(", komplexeListe=").append(komplexeListe);
-		sb.append('}');
-		return sb.toString();
-	}
+    public RootDatenTypen() {
+        double d = Math.random();
+        setTxt("Text 4: " + d);
+        setZahl((int) (d * 6 + 1));
+        setGleitZahl((float) d);
+        datum = LocalDate.now();
+        liste = new ArrayList<>();
+        liste.add("Eins");
+        liste.add("Zwei");
+        liste.add("Drei");
 
+        mappe = new HashMap<>();
+        mappe.put(1, "Jan");
+        mappe.put(2, "Feb");
+        mappe.put(3, "Mar");
+        mappe.put(4, "Apr");
+
+        komplexeListe = new ArrayList<>();
+        komplexeListe.add(new TeilDatenTypen());
+        komplexeListe.add(new TeilDatenTypen());
+        komplexeListe.add(new TeilDatenTypen());
+    }
+
+    public String getTxt() {
+        return txt;
+    }
+
+    public void setTxt(String txt) {
+        this.txt = txt;
+    }
+
+    public int getZahl() {
+        return zahl;
+    }
+
+    public void setZahl(int zahl) {
+        this.zahl = zahl;
+    }
+
+    public float getGleitZahl() {
+        return gleitZahl;
+    }
+
+    public void setGleitZahl(float gleitZahl) {
+        this.gleitZahl = gleitZahl;
+    }
+
+    public LocalDate getDatum() {
+        System.out.println("Get Datum");
+        return datum;
+    }
+
+    public void setDatum(LocalDate datum) {
+        this.datum = datum;
+    }
+
+    public List<String> getListe() {
+        return liste;
+    }
+
+    public void setListe(List<String> liste) {
+        this.liste = liste;
+    }
+
+    public Map<Integer, String> getMappe() {
+        return mappe;
+    }
+
+    public void setMappe(Map<Integer, String> mappe) {
+        this.mappe = mappe;
+    }
+
+    public List<TeilDatenTypen> getKomplexeListe() {
+        return komplexeListe;
+    }
+
+    public void setKomplexeListe(List<TeilDatenTypen> komplexeListe) {
+        this.komplexeListe = komplexeListe;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("DatenObjekt{");
+        sb.append("txt='").append(txt).append('\'');
+        sb.append(", zahl=").append(zahl);
+        sb.append(", gleitZahl=").append(gleitZahl);
+        sb.append(", datum=").append(datum);
+        sb.append(", liste=").append(liste);
+        sb.append(", mappe=").append(mappe);
+        sb.append(", komplexeListe=").append(komplexeListe);
+        sb.append('}');
+        return sb.toString();
+    }
 
 }
