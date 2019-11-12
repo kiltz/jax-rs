8bf524af2c8e5945bd78c2fa6c0672501e0b8ebd
Friedrich Kiltz <Friedrich.Kiltz@ofdka.bwl.de>
2019-11-12 15:23:54 +0100

    Grundgerüst des Projektes

---
+++ projekt/vorgang-main/src/main/java/de/kiltz/vorgang/main/data/VorgangInfo.java
@@ -0,0 +1,93 @@
+package de.kiltz.vorgang.main.data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author tz (F0290158)
+ */
+public class VorgangInfo {
+
+    private Long id;
+    private String name;
+    private String beschreibung;
+    private LocalDateTime erstelltAm;
+    private String erstelltVon;
+    private LocalDateTime letzteAenderung;
+    private String letzteAenderungVon;
+    private Status status;
+
+    public VorgangInfo() {
+    }
+
+    public VorgangInfo(String name, String beschreibung, String erstelltVon) {
+        this.name = name;
+        this.beschreibung = beschreibung;
+        this.erstelltVon = erstelltVon;
+        erstelltAm = LocalDateTime.now();
+        status = Status.NEU;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public LocalDateTime getErstelltAm() {
+        return erstelltAm;
+    }
+
+    public void setErstelltAm(LocalDateTime erstelltAm) {
+        this.erstelltAm = erstelltAm;
+    }
+
+    public String getErstelltVon() {
+        return erstelltVon;
+    }
+
+    public void setErstelltVon(String erstelltVon) {
+        this.erstelltVon = erstelltVon;
+    }
+
+    public LocalDateTime getLetzteAenderung() {
+        return letzteAenderung;
+    }
+
+    public void setLetzteAenderung(LocalDateTime letzteAenderung) {
+        this.letzteAenderung = letzteAenderung;
+    }
+
+    public String getLetzteAenderungVon() {
+        return letzteAenderungVon;
+    }
+
+    public void setLetzteAenderungVon(String letzteAenderungVon) {
+        this.letzteAenderungVon = letzteAenderungVon;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
---
+++ projekt/vorgang-main/src/main/java/de/kiltz/vorgang/main/service/VorgangsService.java
@@ -0,0 +1,18 @@
+package de.kiltz.vorgang.main.service;
+
+
+import java.util.List;
+
+import de.kiltz.vorgang.main.data.VorgangInfo;
+
+public interface VorgangsService {
+
+    VorgangInfo neu(VorgangInfo v) throws VorgangsException;
+    void aktualisiere(VorgangInfo v) throws VorgangsException;
+    void loesche(Long id);
+
+    VorgangInfo hole(Long id);
+
+    List<VorgangInfo> suche(String name);
+
+}
