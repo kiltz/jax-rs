7d657b0723c4711f1dbc688ff3e93319c2d36b8f
Friedrich <f@kiltz.de>
2019-11-24 12:17:36 +0100

    Mit Ajax

---
+++ jax-rs-server/src/main/java/de/kiltz/rest/basic/AjaxRestService.java
@@ -0,0 +1,26 @@
+package de.kiltz.rest.basic;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("ajax")
+public class AjaxRestService {
+
+	private static int anzahl = 0;
+	
+	@GET
+	@Path("meldungen")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMeldungen() {
+		if (++anzahl == 5) {
+			anzahl = 0;
+			throw new NotFoundException("mag nicht!");
+		}
+		return String.format("%d. Aufruf: Freier Speicher: %,d", anzahl, Runtime.getRuntime().freeMemory());
+	}
+}
